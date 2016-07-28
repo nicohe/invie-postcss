fccfeb15324ddfd9f939911a162c1d4d43b3499a
Nicolas Herrera <h.nico@hotmail.com.ar>
2016-07-28 01:27:10 -0300

    Autoprefixer: Cómo añadir vendor prefixes usando CSS standard

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var browserSync = require('browser-sync').create();
+var autoprefixer = require('autoprefixer');
+
+// Servidor de desarrollo
+gulp.task('serve',function () {
+   browserSync.init({
+       server: {
+           baseDir: './dist'
+        }
+   })
+});
+
+// Tarea para procesar el css
+gulp.task('css',function () {
+
+    var processors = [
+      autoprefixer({browsers: ['> 5%', 'ie 8']})
+    ];
+
+    return gulp.src('./src/*.css')
+                .pipe(postcss(processors))
+                .pipe(gulp.dest('./dist/css'))
+                .pipe(browserSync.stream());
+});
+
+// Tarea para vigilar los cambios
+gulp.task('watch',function () {
+  gulp.watch('./src/*.css', ['css']);
+  gulp.watch('./dist/*.html').on('change', browserSync.reload);
+});
+
+gulp.task('default', ['watch', 'serve']);
